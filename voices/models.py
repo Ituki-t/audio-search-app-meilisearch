9d7cd99217e435defe1f43290556757f719b7eb7
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-02-12 16:49:00 +0900

    models.py

--- voices/models.py
+++ voices/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Voice(models.Model):
+    title = models.CharField(max_length=200)
+    audio_file = models.FileField(upload_to='voices/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
