64ad7fe4ac1b82156c2a61dd1c8e18be92eb6ba0
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-02-26 21:28:15 +0900

    admin

--- voices/admin.py
+++ voices/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Voice
 
 # Register your models here.
+
+
+class VoiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'uploaded_at')
+
+admin.site.register(Voice, VoiceAdmin)
