946b6a9def66e0b68e203ecb047f05ac2c370141
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-02-26 21:29:05 +0900

    テキスト化されたデータを用いてキーワードからオーディオを検索

--- voices/meili_service.py
+++ voices/meili_service.py
@@ -4,13 +4,21 @@ import os
 import dotenv
 dotenv.load_dotenv()
 
-aidio_index = os.getenv('MEILI_INDEX', 'audio_index')
+audio_index = os.getenv('MEILI_INDEX', 'audio_index')
 
 
 def add_audio_documents(voice, text):
     meili = get_meili()
-    meili.index(aidio_index).add_documents([{
+    meili.index(audio_index).add_documents([{
         'title': voice.title,
         'text': text,
         'voice_id': voice.id
     }])
+
+def search_audio_ids(query):
+    meili = get_meili()
+    results = meili.index(audio_index).search(query)
+    hit_ids = []
+    for hit in results['hits']:
+        hit_ids.append(hit['voice_id'])
+    return hit_ids
